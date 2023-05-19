855963f78c30fb9377d826835da6e359180a8a7f
adlib-bitbucket-user <bitbucket-user@ad-lib.io>
2023-05-19 22:39:09 +0800

    localstorage adjust value of chakra ui color mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,11 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider, ColorModeScript, extendTheme, useColorMode, type ThemeConfig } from '@chakra-ui/react'
 import { useEffect } from 'react';
 
+localStorage.setItem('chakra-ui-color-mode', 'dark');
 
-const configChakra: ThemeConfig = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
-
-const { colorMode, toggleColorMode } = useColorMode();
-
-useEffect(() => {
-  if (colorMode === "light") {
-    toggleColorMode();
-  }
-}, []);
-
-const theme = extendTheme(configChakra);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  return <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
 }
