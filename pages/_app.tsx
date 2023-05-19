03b8aae55f0f3e9287e91704f33ffee980e30327
adlib-bitbucket-user <bitbucket-user@ad-lib.io>
2023-05-19 22:23:30 +0800

    useeffect colormode on start set to dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider, ColorModeScript, extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, extendTheme, useColorMode, type ThemeConfig } from '@chakra-ui/react'
+import { useEffect } from 'react';
 
 
 const configChakra: ThemeConfig = {
   useSystemColorMode: false,
   initialColorMode: "dark",
 };
+
+const { colorMode, toggleColorMode } = useColorMode();
+
+useEffect(() => {
+  if (colorMode === "light") {
+    toggleColorMode();
+  }
+}, []);
 
 const theme = extendTheme(configChakra);
 
