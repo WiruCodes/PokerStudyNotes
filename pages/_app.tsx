a46a1505db284752ca8f343ced5843ac31ae0fb6
adlib-bitbucket-user <bitbucket-user@ad-lib.io>
2023-05-19 22:15:14 +0800

    adjust color settings bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+
+const configChakra: ThemeConfig = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme(configChakra);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider>
+  return <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
 }
